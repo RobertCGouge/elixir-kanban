b46c5588b5d924fe54ca930930b7ea63fa0fd05e
Robert Gouge <robert.c.gouge@gmail.com>
2025-08-25 09:41:47 -0500

    format code to fix failing test

--- config/test.exs
+++ config/test.exs
@@ -8,8 +8,7 @@ config :elixir_kanban, ElixirKanbanWeb.Endpoint,
   server: false
 
 # In test we don't send emails.
-config :elixir_kanban, ElixirKanban.Mailer,
-  adapter: Swoosh.Adapters.Test
+config :elixir_kanban, ElixirKanban.Mailer, adapter: Swoosh.Adapters.Test
 
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false
--- test/elixir_kanban_web/controllers/error_html_test.exs
+++ test/elixir_kanban_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule ElixirKanbanWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(ElixirKanbanWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(ElixirKanbanWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
